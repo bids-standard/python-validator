4efb4038a91b2d5711e9e5dad179493fdb3079bc
Chris Markiewicz <effigies@gmail.com>
2024-05-27 18:54:20 -0400

    feat: Create validation context classes from schema.meta.context

---
+++ src/bids_validator/context.py
@@ -0,0 +1,36 @@
+from .context_generator import get_schema, load_schema_into_namespace
+
+schema = get_schema()
+load_schema_into_namespace(schema['meta']['context']['context'], globals(), 'Context')
+
+
+__all__ = [  # noqa: F822
+    'Context',
+    'Schema',
+    'Dataset',
+    'Dataset_description',
+    'Tree',
+    'Subjects',
+    'Subject',
+    'Sessions',
+    'Entities',
+    'Sidecar',
+    'Associations',
+    'Events',
+    'Aslcontext',
+    'M0scan',
+    'Magnitude',
+    'Magnitude1',
+    'Bval',
+    'Bvec',
+    'Channels',
+    'Coordsystem',
+    'Columns',
+    'Json',
+    'Gzip',
+    'Nifti_header',
+    'Dim_info',
+    'Xyzt_units',
+    'Ome',
+    'Tiff',
+]

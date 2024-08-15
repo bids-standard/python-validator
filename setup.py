58732d1ab149886c941bd1d768897f0ea1bf0f60
Chris Markiewicz <effigies@gmail.com>
2024-08-15 09:44:48 -0400

    chore(sty): Check/format setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-from setuptools import setup
 import versioneer
+from setuptools import setup
 
-setup(version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass())
+setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())

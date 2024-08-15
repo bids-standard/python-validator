06d698b8c1f4ee2501baf3584913bcb9faac91d3
Remi Gau <remi_gau@hotmail.com>
2024-08-15 13:24:27 +0200

    split positive and negative tests

--- src/bids_validator/test_bids_validator.py
+++ src/bids_validator/test_bids_validator.py
@@ -60,184 +60,239 @@ def test_datasets(validator, fname):
 
 
 @pytest.mark.parametrize(
-    ('fname', 'matches'),
+    ('fname'),
     [
-        ('/T1w.json', True),
-        ('/dataset_description.json', True),
-        ('/README', True),
-        ('/README.md', True),
-        ('/README.rst', True),
-        ('/CHANGES', True),
-        ('/participants.tsv', True),
-        ('/participants.json', True),
-        ('/sub-01/anat/sub-01_T1w.nii.gz', False),
-        ('/RADME', False),  # typo
-        ('/CANGES', False),  # typo
+        ('/T1w.json'),
+        ('/dataset_description.json'),
+        ('/README'),
+        ('/README.md'),
+        ('/README.rst'),
+        ('/CHANGES'),
+        ('/participants.tsv'),
+        ('/participants.json'),
     ],
 )
-def test_top_level(validator, fname, matches):
+def test_top_level_true(validator, fname):
     """Test that is_top_level returns true for top-level files."""
-    assert validator.is_top_level(fname) is matches
+    assert validator.is_top_level(fname) is True
 
 
 @pytest.mark.parametrize(
-    ('fname', 'matches'),
+    ('fname'),
     [
-        ('/code/', True),
-        ('/derivatives/', True),
-        ('/sourcedata/', True),
-        ('/stimuli/', True),
-        ('/sourcedata/unstructured_data.nii.gz', True),
-        ('/sourcedata/dicom_dir/xyz.dcm', True),
-        ('/code/my_analysis/analysis.py', True),
-        ('/derivatives/preproc/sub-01/anat/sub-01_desc-preproc_T1w.nii.gz', True),
-        ('/stimuli/pic.jpg', True),
-        ('/sub-01/anat/sub-01_T1w.nii.gz', False),
-        ('/CODE/', False),
-        ('/derivatves/', False),
-        ('/source/', False),
-        ('/stimli/', False),
-        ('/.git/', False),
+        ('/sub-01/anat/sub-01_T1w.nii.gz'),
+        ('/RADME'),  # typo
+        ('/CANGES'),  # typo
     ],
 )
-def test_associated_data(validator, fname, matches):
-    """Test that is_associated_data returns true for associated data."""
-    assert validator.is_associated_data(fname) is matches
+def test_top_level(validator, fname):
+    """Test that is_top_level returns false for non top-level files."""
+    assert validator.is_top_level(fname) is False
 
 
 @pytest.mark.parametrize(
-    ('fname', 'matches'),
+    ('fname'),
     [
-        ('/sub-01/ses-1/sub-01_ses-1_scans.tsv', True),
-        ('/sub-01/ses-1/sub-01_ses-1_scans.json', True),
-        ('/sub-01/sub-01_scans.tsv', True),
-        ('/sub-01/sub-01_scans.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_task-rest_bold.json', True),
-        ('/sub-01/sub-01_task-rest_bold.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_asl.json', True),
-        ('/sub-01/sub-01_asl.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_pet.json', True),
-        ('/sub-01/sub-01_pet.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_proc-test_channels.tsv', True),
-        ('/sub-01/ses-1/sub-01_ses-1_channels.json', True),
-        ('/sub-01/sub-01_proc-test_channels.tsv', True),
-        ('/sub-01/sub-01_channels.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_space-CapTrak_electrodes.tsv', True),
-        ('/sub-01/ses-1/sub-01_ses-1_coordsystem.json', True),
-        ('/sub-01/sub-01_space-CapTrak_electrodes.tsv', True),
-        ('/sub-01/sub-01_coordsystem.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_motion.json', True),
-        ('/sub-01/sub-01_motion.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_TEM.json', True),
-        ('/sub-01/sub-01_TEM.json', True),
-        ('/sub-01/ses-1/sub-01_ses-1_nirs.json', True),
-        ('/sub-01/sub-01_nirs.json', True),
-        ('/sub-01/sub-01_dwi.bval', True),
-        ('/sub-01/sub-01_dwi.bvec', True),
-        ('/sub-01/sub-01_dwi.json', True),
-        ('/sub-01/sub-01_run-01_dwi.bval', True),
-        ('/sub-01/sub-01_run-01_dwi.bvec', True),
-        ('/sub-01/sub-01_run-01_dwi.json', True),
-        ('/sub-01/sub-01_acq-singleband_dwi.bval', True),
-        ('/sub-01/sub-01_acq-singleband_dwi.bvec', True),
-        ('/sub-01/sub-01_acq-singleband_dwi.json', True),
-        ('/sub-01/sub-01_acq-singleband_run-01_dwi.bval', True),
-        ('/sub-01/sub-01_acq-singleband_run-01_dwi.bvec', True),
-        ('/sub-01/sub-01_acq-singleband_run-01_dwi.json', True),
-        ('/sub-01/ses-test/sub-01_ses-test_dwi.bval', True),
-        ('/sub-01/ses-test/sub-01_ses-test_dwi.bvec', True),
-        ('/sub-01/ses-test/sub-01_ses-test_dwi.json', True),
-        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.bval', True),
-        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.bvec', True),
-        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.json', True),
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi.bval', True),
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi.bvec', True),
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi.json', True),
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.bval', True),
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.bvec', True),
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.json', True),
+        ('/code/'),
+        ('/derivatives/'),
+        ('/sourcedata/'),
+        ('/stimuli/'),
+        ('/sourcedata/unstructured_data.nii.gz'),
+        ('/sourcedata/dicom_dir/xyz.dcm'),
+        ('/code/my_analysis/analysis.py'),
+        ('/derivatives/preproc/sub-01/anat/sub-01_desc-preproc_T1w.nii.gz'),
+        ('/stimuli/pic.jpg'),
+    ],
+)
+def test_associated_data_true(validator, fname):
+    """Test that is_associated_data returns true for associated data."""
+    assert validator.is_associated_data(fname) is True
+
+
+@pytest.mark.parametrize(
+    ('fname'),
+    [
+        ('/stimuli/pic.jpg'),
+        ('/sub-01/anat/sub-01_T1w.nii.gz'),
+        ('/CODE/'),
+        ('/derivatves/'),
+        ('/source/'),
+        ('/stimli/'),
+        ('/.git/'),
+    ],
+)
+def test_associated_data_false(validator, fname):
+    """Test that is_associated_data returns false for associated data."""
+    assert validator.is_associated_data(fname) is False
+
+
+@pytest.mark.parametrize(
+    ('fname'),
+    [
+        ('/sub-01/ses-1/sub-01_ses-1_scans.tsv'),
+        ('/sub-01/ses-1/sub-01_ses-1_scans.json'),
+        ('/sub-01/sub-01_scans.tsv'),
+        ('/sub-01/sub-01_scans.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_task-rest_bold.json'),
+        ('/sub-01/sub-01_task-rest_bold.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_asl.json'),
+        ('/sub-01/sub-01_asl.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_pet.json'),
+        ('/sub-01/sub-01_pet.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_proc-test_channels.tsv'),
+        ('/sub-01/ses-1/sub-01_ses-1_channels.json'),
+        ('/sub-01/sub-01_proc-test_channels.tsv'),
+        ('/sub-01/sub-01_channels.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_space-CapTrak_electrodes.tsv'),
+        ('/sub-01/ses-1/sub-01_ses-1_coordsystem.json'),
+        ('/sub-01/sub-01_space-CapTrak_electrodes.tsv'),
+        ('/sub-01/sub-01_coordsystem.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_motion.json'),
+        ('/sub-01/sub-01_motion.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_TEM.json'),
+        ('/sub-01/sub-01_TEM.json'),
+        ('/sub-01/ses-1/sub-01_ses-1_nirs.json'),
+        ('/sub-01/sub-01_nirs.json'),
+        ('/sub-01/sub-01_dwi.bval'),
+        ('/sub-01/sub-01_dwi.bvec'),
+        ('/sub-01/sub-01_dwi.json'),
+        ('/sub-01/sub-01_run-01_dwi.bval'),
+        ('/sub-01/sub-01_run-01_dwi.bvec'),
+        ('/sub-01/sub-01_run-01_dwi.json'),
+        ('/sub-01/sub-01_acq-singleband_dwi.bval'),
+        ('/sub-01/sub-01_acq-singleband_dwi.bvec'),
+        ('/sub-01/sub-01_acq-singleband_dwi.json'),
+        ('/sub-01/sub-01_acq-singleband_run-01_dwi.bval'),
+        ('/sub-01/sub-01_acq-singleband_run-01_dwi.bvec'),
+        ('/sub-01/sub-01_acq-singleband_run-01_dwi.json'),
+        ('/sub-01/ses-test/sub-01_ses-test_dwi.bval'),
+        ('/sub-01/ses-test/sub-01_ses-test_dwi.bvec'),
+        ('/sub-01/ses-test/sub-01_ses-test_dwi.json'),
+        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.bval'),
+        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.bvec'),
+        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.json'),
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi.bval'),
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi.bvec'),
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi.json'),
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.bval'),
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.bvec'),
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.json'),
+    ],
+)
+def test_session_level_true(validator, fname):
+    """Test that is_session_level returns true for session level files."""
+    assert validator.is_session_level(fname) is True
+
+
+@pytest.mark.parametrize(
+    ('fname'),
+    [
         # Mismatch sessions
-        ('/sub-01/sub-01_ses-1_scans.tsv', False),
-        ('/sub-01/sub-01_ses-1_scans.json', False),
-        ('/sub-01/ses-1/sub-01_ses-2_scans.tsv', False),
+        ('/sub-01/sub-01_ses-1_scans.tsv'),
+        ('/sub-01/sub-01_ses-1_scans.json'),
+        ('/sub-01/ses-1/sub-01_ses-2_scans.tsv'),
         # File-level
-        ('/sub-01/ses-1/func/sub-01_ses-1_task-rest_bold.nii.gz', False),
-        ('/sub-01/anat/sub-01_T1w.nii.gz', False),
-        ('/sub-01/01_dwi.bvec', False),  # missed subject suffix
-        ('/sub-01/sub_dwi.json', False),  # missed subject id
-        ('/sub-01/sub-01_23_run-01_dwi.bval', False),  # wrong _23_
-        ('/sub-01/sub-01_run-01_dwi.vec', False),  # wrong extension
-        ('/sub-01/sub-01_run-01_dwi.jsn', False),  # wrong extension
-        ('/sub-01/sub-01_acq_dwi.bval', False),  # missed suffix value
-        ('/sub-01/sub-01_acq-23-singleband_dwi.bvec', False),  # redundant -23-
-        ('/sub-01/anat/sub-01_acq-singleband_dwi.json', False),  # redundant /anat/
+        ('/sub-01/ses-1/func/sub-01_ses-1_task-rest_bold.nii.gz'),
+        ('/sub-01/anat/sub-01_T1w.nii.gz'),
+        ('/sub-01/01_dwi.bvec'),  # missed subject suffix
+        ('/sub-01/sub_dwi.json'),  # missed subject id
+        ('/sub-01/sub-01_23_run-01_dwi.bval'),  # wrong _23_
+        ('/sub-01/sub-01_run-01_dwi.vec'),  # wrong extension
+        ('/sub-01/sub-01_run-01_dwi.jsn'),  # wrong extension
+        ('/sub-01/sub-01_acq_dwi.bval'),  # missed suffix value
+        ('/sub-01/sub-01_acq-23-singleband_dwi.bvec'),  # redundant -23-
+        ('/sub-01/anat/sub-01_acq-singleband_dwi.json'),  # redundant /anat/
         (
             '/sub-01/sub-01_recrod-record_acq-singleband_run-01_dwi.bval',
             False,
         ),  # redundant record-record_
-        ('/sub_01/sub-01_acq-singleband_run-01_dwi.bvec', False),  # wrong /sub_01/
-        ('/sub-01/sub-01_acq-singleband__run-01_dwi.json', False),  # wrong __
-        ('/sub-01/ses-test/sub-01_ses_test_dwi.bval', False),  # wrong ses_test
-        ('/sub-01/ses-test/sb-01_ses-test_dwi.bvec', False),  # wrong sb-01
-        ('/sub-01/ses-test/sub-01_ses-test_dw.json', False),  # wrong modality
-        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.val', False),  # wrong extension
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband.bval', False),  # missed modality
-        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi', False),  # missed extension
-        ('/ses-test/sub-01/sub-01_ses-test_acq-singleband_dwi.json', False),  # wrong dirs order
+        ('/sub_01/sub-01_acq-singleband_run-01_dwi.bvec'),  # wrong /sub_01/
+        ('/sub-01/sub-01_acq-singleband__run-01_dwi.json'),  # wrong __
+        ('/sub-01/ses-test/sub-01_ses_test_dwi.bval'),  # wrong ses_test
+        ('/sub-01/ses-test/sb-01_ses-test_dwi.bvec'),  # wrong sb-01
+        ('/sub-01/ses-test/sub-01_ses-test_dw.json'),  # wrong modality
+        ('/sub-01/ses-test/sub-01_ses-test_run-01_dwi.val'),  # wrong extension
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband.bval'),  # missed modality
+        ('/sub-01/ses-test/sub-01_ses-test_acq-singleband_dwi'),  # missed extension
+        ('/ses-test/sub-01/sub-01_ses-test_acq-singleband_dwi.json'),  # wrong dirs order
         (
             '/sub-01/ses-test/sub-02_ses-test_acq-singleband_run-01_dwi.bval',
             False,
         ),  # wrong sub id in the filename
-        ('/sub-01/sub-01_ses-test_acq-singleband_run-01_dwi.bvec', False),  # ses dir missed
-        ('/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.json', False),  # sub id dir missed
+        ('/sub-01/sub-01_ses-test_acq-singleband_run-01_dwi.bvec'),  # ses dir missed
+        ('/ses-test/sub-01_ses-test_acq-singleband_run-01_dwi.json'),  # sub id dir missed
     ],
 )
-def test_session_level(validator, fname, matches):
-    """Test that is_session_level returns true for session level files."""
-    assert validator.is_session_level(fname) is matches
+def test_session_level_false(validator, fname):
+    """Test that is_session_level returns false for non session level files."""
+    assert validator.is_session_level(fname) is False
 
 
 @pytest.mark.parametrize(
-    ('fname', 'matches'),
+    ('fname'),
     [
-        ('/sub-01/sub-01_sessions.tsv', True),
-        ('/sub-01/sub-01_sessions.json', True),
-        ('/sub-01/anat/sub-01_T1w.nii.gz', False),
-        ('/sub-02/sub-01_sessions.tsv', False),  # wrong sub id in the filename
-        ('/sub-01_sessions.tsv', False),  # missed subject id dir
-        ('/sub-01/sub-01_sesions.tsv', False),  # wrong modality
-        ('/sub-01/sub-01_sesions.ext', False),  # wrong extension
-        ('/sub-01/sub-01_sessions.jon', False),  # wrong extension
+        ('/sub-01/sub-01_sessions.tsv'),
+        ('/sub-01/sub-01_sessions.json'),
     ],
 )
-def test_subject_level(validator, fname, matches):
+def test_subject_level_true(validator, fname):
     """Test that is_subject_level returns true for subject level files."""
-    assert validator.is_subject_level(fname) is matches
+    assert validator.is_subject_level(fname) is True
 
 
 @pytest.mark.parametrize(
-    ('fname', 'matches'),
+    ('fname'),
     [
-        ('/phenotype/measure.tsv', True),
-        ('/phenotype/measure.json', True),
-        ('/sub-01/anat/sub-01_T1w.nii.gz', False),
-        ('/measurement_tool_name.tsv', False),  # missed phenotype dir
-        ('/phentype/measurement_tool_name.josn', False),  # wrong phenotype dir
-        ('/phenotype/measurement_tool_name.jsn', False),  # wrong extension
+        ('/sub-01/anat/sub-01_T1w.nii.gz'),
+        ('/sub-02/sub-01_sessions.tsv'),  # wrong sub id in the filename
+        ('/sub-01_sessions.tsv'),  # missed subject id dir
+        ('/sub-01/sub-01_sesions.tsv'),  # wrong modality
+        ('/sub-01/sub-01_sesions.ext'),  # wrong extension
+        ('/sub-01/sub-01_sessions.jon'),  # wrong extension
     ],
 )
-def test_phenotpic(validator, fname, matches):
+def test_subject_level_false(validator, fname):
+    """Test that is_subject_level returns false for non subject level files."""
+    assert validator.is_subject_level(fname) is False
+
+
+@pytest.mark.parametrize(
+    ('fname'),
+    [
+        ('/phenotype/measure.tsv'),
+        ('/phenotype/measure.json'),
+        ('/sub-01/anat/sub-01_T1w.nii.gz'),
+        ('/measurement_tool_name.tsv'),  # missed phenotype dir
+        ('/phentype/measurement_tool_name.josn'),  # wrong phenotype dir
+        ('/phenotype/measurement_tool_name.jsn'),  # wrong extension
+    ],
+)
+def test_phenotpic_true(validator, fname):
     """Test that is_phenotypic returns true for phenotypic files."""
-    assert validator.is_phenotypic(fname) is matches
+    assert validator.is_phenotypic(fname) is True
 
 
 @pytest.mark.parametrize(
-    ('fname', 'matches'),
+    ('fname'),
     [
-        ('/sub-01/ses-1/func/sub-01_ses-1_task-rest_bold.nii.gz', True),
-        ('/sub-01/anat/sub-01_T1w.nii.gz', True),
+        ('/sub-01/anat/sub-01_T1w.nii.gz'),
+        ('/measurement_tool_name.tsv'),  # missed phenotype dir
+        ('/phentype/measurement_tool_name.josn'),  # wrong phenotype dir
+        ('/phenotype/measurement_tool_name.jsn'),  # wrong extension
     ],
 )
-def test_file_level(validator, fname, matches):
+def test_phenotpic_false(validator, fname):
+    """Test that is_phenotypic returns false for non phenotypic files."""
+    assert validator.is_phenotypic(fname) is False
+
+
+@pytest.mark.parametrize(
+    ('fname'),
+    [
+        ('/sub-01/ses-1/func/sub-01_ses-1_task-rest_bold.nii.gz'),
+        ('/sub-01/anat/sub-01_T1w.nii.gz'),
+    ],
+)
+def test_file_level(validator, fname):
     """Test that is_file returns true for file level files."""
-    assert validator.is_file(fname) is matches
+    assert validator.is_file(fname) is True

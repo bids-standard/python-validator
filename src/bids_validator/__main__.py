50ffdf300db0898c4f27a4b647a9bad7ad81cf08
Dan Brady <daniel.brady@sheffield.ac.uk>
2025-04-09 16:39:58 +0100

    add vebose flag that shows version

--- src/bids_validator/__main__.py
+++ src/bids_validator/__main__.py
@@ -4,9 +4,14 @@ except ImportError:
     print('⚠️ CLI dependencies are not installed. Install "bids_validator[cli]"')
     raise SystemExit(1) from None
 
+import sys
+from typing_extensions import Annotated
+import importlib.metadata
+
 from bids_validator import BIDSValidator
 from bids_validator.types.files import FileTree
 
+__version__ = importlib.metadata.version(__package__ or __name__)
 app = typer.Typer()
 
 
@@ -48,9 +53,22 @@ def validate(tree: FileTree):
         if not validator.is_bids(path):
             print(f'{path} is not a valid bids filename')
 
+def show_version():
+    """Show bids-validator version
+    """
+    print(f'bids-validator {__version__} (Python {sys.version.split()[0]})')
 
 @app.command()
-def main(bids_path: str):
+def main(
+    bids_path: str,
+    verbose: Annotated[bool, typer.Option(
+        "--verbose", "-v",
+        help="Show verbose output"
+    )] = False,
+) -> None:
+    
+    if verbose: show_version()
+
     root_path = FileTree.read_from_filesystem(bids_path)
 
     validate(root_path)

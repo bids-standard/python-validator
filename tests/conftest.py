eff4294f12c0be47710d261745593e51b077df14
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-04-28 10:10:42 -0400

    test: Add gitignore-test submodule

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,14 @@ def examples() -> Path:
         if not any(ret.iterdir()):
             pytest.skip('bids-examples submodule is not checked out')
     return Path(ret)
+
+
+@pytest.fixture(scope='session')
+def gitignore_test() -> Path:
+    """Get bids-examples from submodule, allow environment variable override."""
+    ret = os.getenv('GITIGNORE_TEST_DIR')
+    if not ret:
+        ret = importlib.resources.files(__spec__.parent) / 'data' / 'gitignore-test'
+        if not any(ret.iterdir()):
+            pytest.skip('gitignore-test submodule is not checked out')
+    return Path(ret)

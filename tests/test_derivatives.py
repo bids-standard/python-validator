67dd6da35c4aa86e4ac8b9d5f06a85e26ab01fdf
Ashley Stewart <a.stewart.au@gmail.com>
2026-03-06 10:01:16 +1000

    Add comments noting derivative-specific features in tests

--- tests/test_derivatives.py
+++ tests/test_derivatives.py
@@ -8,13 +8,13 @@ from bids_validator import BIDSValidator
 @pytest.mark.parametrize(
     'fname',
     [
-        '/sub-04/anat/sub-04_space-MNI152_T1w.nii.gz',
-        '/sub-04/anat/sub-04_space-MNI152_desc-preproc_T1w.nii.gz',
-        '/sub-04/ses-02/func/sub-04_ses-02_task-rest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii',
-        '/sub-04/ses-02/func/sub-04_ses-02_task-nback_run-02_space-T1w_label-brain_mask.nii',
-        '/sub-04/ses-01/func/sub-04_ses-01_task-nback_run-01_space-T1w_desc-preproc_bold.json',
-        '/sub-01/anat/sub-01_res-1_T1w.nii.gz',
-        '/sub-01/anat/sub-01_space-MNI152_dseg.nii.gz',
+        '/sub-04/anat/sub-04_space-MNI152_T1w.nii.gz',  # space
+        '/sub-04/anat/sub-04_space-MNI152_desc-preproc_T1w.nii.gz',  # space + desc
+        '/sub-04/ses-02/func/sub-04_ses-02_task-rest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii',  # space + desc
+        '/sub-04/ses-02/func/sub-04_ses-02_task-nback_run-02_space-T1w_label-brain_mask.nii',  # space + label
+        '/sub-04/ses-01/func/sub-04_ses-01_task-nback_run-01_space-T1w_desc-preproc_bold.json',  # space + desc
+        '/sub-01/anat/sub-01_res-1_T1w.nii.gz',  # res
+        '/sub-01/anat/sub-01_space-MNI152_dseg.nii.gz',  # dseg suffix
     ],
 )
 def test_derivative_filenames_are_bids(fname: str) -> None:
@@ -59,8 +59,8 @@ def test_parse_derivative_entities(fname: str, expected: dict[str, str]) -> None
 @pytest.mark.parametrize(
     'fname',
     [
-        '/sub-01/anat/sub-01_space-MNI152_desc-preproc_T1w.exe',
-        '/sub-01/anat/sub-01_space-MNI152_desc-preproc_T1w.niigz',
+        '/sub-01/anat/sub-01_space-MNI152_desc-preproc_T1w.exe',  # wrong extension
+        '/sub-01/anat/sub-01_space-MNI152_desc-preproc_T1w.niigz',  # wrong extension
         '/sub-01/func/sub-01_space-MNI152_desc-preproc_bold.nii.gz',  # missing required task entity
     ],
 )
